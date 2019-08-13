05f23029fd4d39110e82eeec9113fe8e2d2de675
EC2 Default User <ec2-user@ip-172-31-27-174.us-east-2.compute.internal>
2019-08-13 01:24:26 +0000

    routes set, order controller functions updated to match routes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,71 +1,56 @@
 class OrdersController < ApplicationController
-  before_action :set_order, only: [:show, :edit, :update, :destroy]
-
-  # GET /orders
-  # GET /orders.json
-  def index
-    @orders = Order.all
-  end
-
-  # GET /orders/1
-  # GET /orders/1.json
-  def show
-  end
-
-  # GET /orders/new
-  def new
-    @order = Order.new
-  end
-
-  # GET /orders/1/edit
-  def edit
-  end
 
   # POST /orders
   # POST /orders.json
   def create
     @order = Order.new(order_params)
 
-    respond_to do |format|
       if @order.save
-        format.html { redirect_to @order, notice: 'Order was successfully created.' }
-        format.json { render :show, status: :created, location: @order }
+        render json: @order, status: 200
       else
-        format.html { render :new }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
+        render json: @order.errors.messages, status: 400
       end
+
+  end
+
+  # GET "orders/:id"
+  def retrieveById
+    #find throws an error instead of returning nil
+    @order = Order.find(params[:id]) rescue nil
+
+    #success
+    if @item
+      render json: @order , status: 200
+    #failure
+    else
+      render json: "", status: 404
     end
   end
 
-  # PATCH/PUT /orders/1
-  # PATCH/PUT /orders/1.json
-  def update
-    respond_to do |format|
-      if @order.update(order_params)
-        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
-        format.json { render :show, status: :ok, location: @order }
-      else
-        format.html { render :edit }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
+  def retrieveByOther
+    
+    #customer id to search for, if given, otherwise determine the id from email.
+    targetId = params[:customerId]
+    if targetId == nil
+      @customer = Customer.find_by(email: params[:email])
+      targetId = @customer[:id]
     end
+
+    @orders = Order.find_by(customerId: targetId)
+    
+   if @orders
+     #success
+     render json: @orders , status: 200
+   else
+     #failure 
+     head 404
+   end
+     
   end
 
-  # DELETE /orders/1
-  # DELETE /orders/1.json
-  def destroy
-    @order.destroy
-    respond_to do |format|
-      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_order
-      @order = Order.find(params[:id])
-    end
+
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :orders
+  #resources :orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post 'orders', to: 'order#create'
+  get 'orders/:id', to: 'order#retrieveById'
+  get 'orders', to: 'order#retrieveByOther'
+  #/orders?customerId=nnn
+  #/orders?email=nn@nnnn
 end
