1c74bbe15d9e48ea55e8d613ad387ae455f21667
EC2 Default User <ec2-user@ip-172-31-27-174.us-east-2.compute.internal>
2019-08-13 02:36:53 +0000

    Routes added, and controller methods updated to reflect changes. Some controller functions implemented but untested

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,54 @@
-class OrdersController < ApplicationController
+require 'httparty'   
+require 'json'
 
+class OrdersController < ApplicationController
+  include Httparty
   # POST /orders
   # POST /orders.json
   def create
-    @order = Order.new(order_params)
-
+    fail = false
+    itemId = order_params[:itemId]
+    customerId = order_params[:customerId]
+    
+    base_uri 'http://localhost:8082'
+    uri = "/items/%d" % [itemId]
+    response = get uri
+    
+    #Check if item of this id was fetched correctly
+    if response.code == 200
+      @item = response.body
+    else
+      fail = true
+    end
+    
+    base_uri 'http://localhost:8081'
+    uri = "/customers?id=%d" % [customerId]
+    response = get uri
+    
+    #Check if customer of this id was fetched correctly
+    if response.code == 200
+      @customer = response.body
+    else
+      fail = true
+    end
+    
+    #If item and customer are found, create the order, otherwise, give a 404 not found error
+    if !fail 
+      description = @item[:description]
+      price = @item[:price]
+      @customer.award = '%.2f' % (0.10 * (@customer.lastOrder + @customer.lastOrder2 + @customer.lastOrder3)/3)
+      award = @customer[:award]
+      total = price - award
+  
+      @order = Order.new({itemId: itemId, customerId: customerId, description: description, price: price, award: award, total: total})
       if @order.save
-        render json: @order, status: 200
+        render json: @order, status: 201
       else
         render json: @order.errors.messages, status: 400
       end
+    else
+      head 404
+    end
 
   end
 
@@ -54,6 +93,6 @@ class OrdersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
-      params.require(:order).permit(:itemId, :customerId, :description, :price, :award, :total)
+      params.require(:order).permit(:itemId, :customerId)
     end
 end
